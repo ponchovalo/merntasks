05e5c04be111b3c785b58cc37740db39d54d6c31
Ildefonso Valenzuela <ponchovalo@outlook.com>
2020-07-12 21:09:23 -0500

    mostrar informacion de usuario

--- cliente/src/components/layout/Barra.js
+++ cliente/src/components/layout/Barra.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect, useContext} from 'react';
+import AuthContext from '../../context/autenticacion/authContext'
 
 const Barra = () => {
+    // extraer la informacion de autenticacion
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado  } = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado()
+    }, [])
     return ( 
         <header className="app-header">
-            <p className="nombre-usuario">Hola <span>Ildefonso Valenzuela</span></p>
+            {usuario ? <p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> : null}
+            
             <nav className="nav-principal">
                 <a href="#!">Cerrar Sesion</a>
             </nav>
