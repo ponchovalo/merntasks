c13f05c694913bc6a551bb4d003eed5b7fba85e2
Ildefonso Valenzuela <ponchovalo@outlook.com>
2020-07-12 22:30:54 -0500

    protegiendo rutas

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -9,6 +9,7 @@ import TareaState from './context/tareas/tareaState'
 import AlertaState from './context/alertas/alertaState'
 import AuthState from './context/autenticacion/authState'
 import tokenAuth from './config/token'
+import RutaPrivada from './components/rutas/RutaPrivada'
 
 // Revisar si hay token
 const token = localStorage.getItem('token');
@@ -27,7 +28,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Login}/>
                 <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
-                <Route exact path="/proyectos" component={Proyectos}/>
+                <RutaPrivada exact path="/proyectos" component={Proyectos}/>
               </Switch>
             </Router>
           </AuthState>
--- cliente/src/context/autenticacion/authState.js
+++ cliente/src/context/autenticacion/authState.js
@@ -18,7 +18,8 @@ const AuthState = props => {
         token: localStorage.getItem('token'),
         autenticado: null,
         usuario: null,
-        mensaje: null
+        mensaje: null,
+        cargando: true
     }
 
     const [state, dispatch] = useReducer(AuthReducer, initialState);
@@ -94,7 +95,7 @@ const AuthState = props => {
             })
         }
     }
-    
+
     // Cierra la sesion del usuario
     const cerraSesion = () => {
         dispatch({
@@ -109,6 +110,7 @@ const AuthState = props => {
                 autenticado: state.autenticado,
                 usuario: state.usuario,
                 mensaje: state.mensaje,
+                cargando: state.cargando,
                 registrarUsuario,
                 iniciarSesion,
                 usuarioAutenticado,
